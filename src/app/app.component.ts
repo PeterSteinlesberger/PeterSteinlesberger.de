6987c4fe3c45b5f0f0d6559ae857ee94b79101f5
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-12-30 19:30:54 +0100

    slideshow added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Portfolio_Webpage';
+export class AppComponent implements OnInit {
+ images = ['sourceCodeArt.jpg', 'software-development.jpg', 'dnaBinary.jpg', 'codeScreenSplitted.png'];
+currentImage = 0;
+
+ngOnInit() {
+this.updateImage();
+}
+
+
+
+updateImage() {
+
+  setInterval(() => {
+    this.currentImage++;
+    if(this.currentImage == this.images.length) {
+      this.currentImage = 0;
+    }
+ // = this.currentImage % this.images.length;
+  }, 8000);
+}
+
+
 }
